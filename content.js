5494b3bc3c79edcfe267119e5e31db71be541151
hatprab <hatprab@gmail.com>
2019-02-16 03:05:52 +0530

    Added background Script

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-console.log("I m live")
+console.log("Shopify admin tool is loaded")
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage( message , sender , sendResponse ){
+    $('.line-items-section .aspect-ratio--square--50').css({width:"150px",height:"150px"})
+}
